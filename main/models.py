1ebc7b299bc6f96fcc5a6eaee27aaf9b0e744f84
Dan <dan.balan0712@gmail.com>
2015-08-25 18:12:10 -0500

    add footer

--- main/models.py
+++ main/models.py
@@ -16,6 +16,20 @@ class Client(models.Model):
     user = models.ForeignKey(User)
 
 
+INVESTMENT_TYPES = (("BONDS", "BONDS"), ("STOCKS", "STOCKS"))
+
+
+SUPER_ASSET_CLASSES = (
+    # EQUITY
+    ("EQUITY_AU", "EQUITY_AU"), ("EQUITY_US", "EQUITY_US"), ("EQUITY_EU", "EQUITY_EU"), ("EQUITY_EM", "EQUITY_EM"),
+    # FIXED_INCOME
+    ("FIXED_INCOME_AU", "FIXED_INCOME_AU"), ("FIXED_INCOME_US", "FIXED_INCOME_US"),
+    ("FIXED_INCOME_EU", "FIXED_INCOME_EU"), ("FIXED_INCOME_EM", "FIXED_INCOME_EM"),
+
+)
+
+
+
 class AssetClass(models.Model):
     name = models.CharField(max_length=255)
     display_order = models.PositiveIntegerField()
@@ -25,7 +39,7 @@ class AssetClass(models.Model):
     asset_class_explanation = models.TextField(blank=True, default="", null=False)
     tickers_explanation = models.TextField(blank=True, default="", null=False)
     display_name = models.CharField(max_length=255, blank=False, null=False)
-    investment_type = models.CharField(max_length=255)
+    investment_type = models.CharField(max_length=255, choices=INVESTMENT_TYPES,  blank=False, null=False)
     super_asset_class = models.CharField(max_length=255)
 
 
@@ -35,5 +49,5 @@ class Ticker(models.Model):
     description = models.TextField(blank=True, default="", null=False)
     ordering = models.IntegerField(blank=True, default="", null=False)
     url = models.URLField()
-    unit_price = models.FloatField(default=1)
+    unit_price = models.FloatField(default=1, editable=False)
     asset_class = models.ForeignKey(AssetClass, related_name="tickers")
