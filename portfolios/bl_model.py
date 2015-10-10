0a1cbc59c494472b3851a351e92bdd38348f2277
Dan <dan.balan0712@gmail.com>
2015-10-09 20:43:30 -0500

    fix allocation

--- portfolios/bl_model.py
+++ portfolios/bl_model.py
@@ -39,7 +39,7 @@ def fitness(W, R, C, r, assets_type, allocation, constrains):
         c += v
         jac += mmult*new_jac
 
-    func = mmult*(c + (sum(W)-1)**2 + (dot(assets_type, W)-allocation)**2) - mean_1 + sqrt(var)
+    func = mmult*(c + (sum(W)-1)**2 + (dot(assets_type, W)-allocation)**2) - 10*mean_1 +  sqrt(var)
 
     var_jac = var_gradient(W, C)*1/2/sqrt(var)
     jac += var_jac
@@ -47,7 +47,7 @@ def fitness(W, R, C, r, assets_type, allocation, constrains):
     jac += mmult*2*(sum(W) -1)
     # jac allocation
     jac += mmult*2*(dot(assets_type, W)-allocation)*assets_type
-    jac -= R
+    jac -= 10*R
     # jacobian sharpe ratio 
     return func, jac 
 
