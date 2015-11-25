5b985ea252910e9b934a950780e0cfde2a8989b4
Dan <dan.balan0712@gmail.com>
2015-11-24 19:40:22 -0500

    added wordpress

--- devop/nginx/entrypoint.sh
+++ devop/nginx/entrypoint.sh
@@ -5,6 +5,7 @@ python configure_nginx.py
 
 mv demo.conf.new /etc/nginx/conf.d/demo.conf
 mv app.conf.new /etc/nginx/conf.d/app.conf
+cat wp.conf.new
 mv wp.conf.new /etc/nginx/conf.d/wp.conf
 
 nginx -g "daemon off;"
