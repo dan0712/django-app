c003839eca471551cdaee08732e32a017f01a776
Dan <dan.balan0712@gmail.com>
2016-02-09 06:20:58 -0500

    Added /api/v1/user/level/ to check if user is client or advisor or both.

---
+++ api/v1/user/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from api.v1.user.views import *
+
+urlpatterns = patterns('',
+
+                       (r'^api/v1/user/level/?$', APIUserLevel.as_view()),
+
+)
---
+++ api/v1/user/views.py
@@ -0,0 +1,49 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from api.v1.utils.api_exceptions import *
+from api.v1.utils.api_responses import *
+from api.v1.utils.api_serializers import *
+
+
+class APIUserLevel(APIView):
+    def get(self, request, format=None):
+
+        try:
+
+            # by default lets just set both false.
+            client = False
+            advisor = False
+
+            user = self.request.user
+
+            # check if they have client node
+            if hasattr(user, 'client'):
+                # now check if client node is not NULL
+                if user.client is not None:
+                    client = True
+
+            # check if they have advisor node
+            if hasattr(user, 'advisor'):
+                # now check if advisor node is not NULL
+                if user.advisor is not None:
+                    advisor = True
+
+        except Exception as e:
+
+            if hasattr(e, 'detail'):
+
+                response = e.detail
+
+            else:
+                response = dict()
+                response['message'] = str(e.message)
+                response['status'] = 'error'
+
+            raise ExceptionDefault(detail=response)
+
+        content = {
+            'client': client,
+            'advisor': advisor,
+        }
+
+        return Response(content)
