b8a1b4cb2428657193d982bb1f04666d0aa250f6
Dan <dan.balan0712@gmail.com>
2016-02-09 10:07:08 +0100

    Changes to allow CD and upgrading withour restarting web server.

--- startup_utils.py
+++ startup_utils.py
@@ -12,25 +12,27 @@ environment = os.environ['ENVIRONMENT']
 def create_db():
     conn = psycopg2.connect(database='postgres',
                             user='postgres',
-                            password=os.environ['DB_ENV_POSTGRES_PASSWORD'],
-                            host=os.environ["DB_PORT_5432_TCP_ADDR"],
-                            port=os.environ["DB_PORT_5432_TCP_PORT"])
+                            password=os.environ['POSTGRES_PASSWORD'],
+                            host=os.environ.get('DB_HOST', "postgres"),
+                            port=int(os.environ.get('DB_PORT', 5432)))
     conn.autocommit = True
     cur = conn.cursor()
-    cur.execute("SELECT 1 FROM pg_database WHERE datname='betasmartz_{}'".format(environment))
+    dbname = os.environ.get('DB_NAME', 'betasmartz_{}'.format(environment))
+    dbuser = os.environ.get('DB_USER', 'betasmartz_{}'.format(environment))
+    cur.execute("SELECT 1 FROM pg_database WHERE datname='{}'".format(dbname))
     if cur.fetchone() is None:
-        logger.info("Creating new database for environment: {}".format(environment))
-        cur.execute('CREATE DATABASE betasmartz_{}'.format(environment))
+        logger.info("Creating new database: {} for environment: {}".format(dbname, environment))
+        cur.execute('CREATE DATABASE {}'.format(dbname))
     else:
-        logger.info("Reusing existing database for environment: {}".format(environment))
+        logger.info("Reusing existing database: {} for environment: {}".format(dbname, environment))
     conn.autocommit = False
-    cur.execute("SELECT 1 FROM pg_roles WHERE rolname='betasmartz_{}'".format(environment))
+    cur.execute("SELECT 1 FROM pg_roles WHERE rolname='{}'".format(dbuser))
     if cur.fetchone() is None:
-        logger.info("Creating new user for environment: {}".format(environment))
-        cur.execute("CREATE USER betasmartz_{} ENCRYPTED PASSWORD '{}'".format(environment, os.environ["DB_PASSWORD"]))
-        cur.execute('GRANT ALL PRIVILEGES ON DATABASE betasmartz_{0} TO betasmartz_{0}'.format(environment))
+        logger.info("Creating new user: {} for environment: {}".format(dbuser, environment))
+        cur.execute("CREATE USER {} ENCRYPTED PASSWORD '{}'".format(dbuser, os.environ["DB_PASSWORD"]))
+        cur.execute('GRANT ALL PRIVILEGES ON DATABASE {} TO {}'.format(dbname, dbuser))
     else:
-        logger.info("Reusing existing user for environment: {}".format(environment))
+        logger.info("Reusing existing user: {} for environment: {}".format(dbuser, environment))
     conn.commit()
     conn.close()
 
