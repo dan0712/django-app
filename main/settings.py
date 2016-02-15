5c2e4559ef1192afa9f2a6c50f59d66689f43b88
Dan <dan.balan0712@gmail.com>
2016-02-15 07:53:19 +0300

    enable token auth headers

--- main/settings.py
+++ main/settings.py
@@ -93,6 +93,13 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = (
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+)
 
 ROOT_URLCONF = 'main.urls'
 
