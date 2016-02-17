aee7023e806eb21d63caec01e45cf9e5ca51fe50
Dan <dan.balan0712@gmail.com>
2016-02-17 18:54:59 +0200

    Allow with and without trailing slash

--- api/v1/user/urls.py
+++ api/v1/user/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     url(r'^user/?$', APIUser.as_view(), name='user'), # TODO: bearly needed
     url(r'^user/level/?$', APIUserLevel.as_view(), name='user-level'), # TODO: lets drop it
 
-    url(r'^me/$', views.MeView.as_view(), name='user-me'),
-    url(r'^login/$', views.LoginView.as_view(), name='user-login'),
+    url(r'^me/?$', views.MeView.as_view(), name='user-me'),
+    url(r'^login/?$', views.LoginView.as_view(), name='user-login'),
     # reserved # url(r'^register/$', views.RegisterView.as_view(), name='user-register'),
 
     # reserved # url(r'^register/reset/$', views.ResetView.as_view(), name='user-reset'),
