fd02eccc9a3ae397f70c5d746a79b84962e201e5
Dan <dan.balan0712@gmail.com>
2016-02-19 15:20:37 +0100

    Migration disaster. Start from scratch.

--- advisors/migrations/0001_initial.py
+++ advisors/migrations/0001_initial.py
@@ -8,42 +8,53 @@ import django_localflavor_au.models
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('main', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='BulkInvestorTransfer',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('approved', models.BooleanField(default=False)),
                 ('approved_at', models.DateTimeField(null=True)),
                 ('create_at', models.DateTimeField(auto_now_add=True)),
-                ('signature', models.FileField(upload_to='')),
+                ('signatures', models.FileField(upload_to='')),
+                ('firm', models.ForeignKey(to='main.Firm', editable=False)),
+                ('from_advisor', models.ForeignKey(to='main.Advisor')),
+                ('investors', models.ManyToManyField(to='main.Client')),
+                ('to_advisor', models.ForeignKey(to='main.Advisor', verbose_name='To Advisor', related_name='bulk_transfer_to_advisors')),
             ],
         ),
         migrations.CreateModel(
             name='ChangeDealerGroup',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('approved', models.BooleanField(default=False)),
                 ('create_at', models.DateTimeField(auto_now_add=True)),
-                ('approved_at', models.DateTimeField(null=True)),
+                ('approved_at', models.DateTimeField(null=True, blank=True)),
                 ('work_phone', django_localflavor_au.models.AUPhoneNumberField(max_length=10)),
-                ('letter_previous_group', models.FileField(upload_to='')),
-                ('letter_new_group', models.FileField(upload_to='')),
+                ('new_email', models.EmailField(max_length=254)),
+                ('letter_previous_group', models.FileField(upload_to='', verbose_name='Prev. Group Letter')),
+                ('letter_new_group', models.FileField(upload_to='', verbose_name='New Group Letter')),
                 ('signature', models.FileField(upload_to='')),
+                ('advisor', models.ForeignKey(to='main.Advisor')),
+                ('new_firm', models.ForeignKey(to='main.Firm', related_name='new_advisors')),
+                ('old_firm', models.ForeignKey(to='main.Firm', related_name='old_advisors')),
             ],
         ),
         migrations.CreateModel(
             name='SingleInvestorTransfer',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('approved', models.BooleanField(default=False)),
                 ('approved_at', models.DateTimeField(null=True)),
                 ('create_at', models.DateTimeField(auto_now_add=True)),
-                ('signature_investor', models.FileField(upload_to='')),
-                ('signature_advisor', models.FileField(upload_to='')),
-                ('signature_joint_investor', models.FileField(upload_to='')),
+                ('signatures', models.FileField(upload_to='')),
+                ('firm', models.ForeignKey(to='main.Firm', editable=False)),
+                ('from_advisor', models.ForeignKey(to='main.Advisor')),
+                ('investor', models.ForeignKey(to='main.Client')),
+                ('to_advisor', models.ForeignKey(to='main.Advisor', verbose_name='To Advisor', related_name='single_transfer_to_advisors')),
             ],
         ),
     ]
