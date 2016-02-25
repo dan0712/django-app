e97d8324208810eb3f29051ff27d176f77c10e2d
Dan <dan.balan0712@gmail.com>
2016-02-25 18:45:59 +0200

    Return collection of objects with ids in asset_features and constraint_comparisons APIs

--- api/v1/settings/views.py
+++ api/v1/settings/views.py
@@ -45,20 +45,28 @@ class SettingsViewSet(ApiViewMixin, viewsets.GenericViewSet):
 
     @list_route(methods=['get'])
     def asset_features(self, request):
-        res = {
-            af.id: {
+        res = [
+            {
+				"id": af.id,
                 "name": af.name,
                 "description": af.description,
-                "values": {
-                    v.id: {
+                "values": [
+                    {
+						"id": v.id,
                         "name": v.name,
                         "description": v.description
                     } for v in af.values.all()
-                }
+                ]
             } for af in AssetFeature.objects.all()
-        }
+        ]
         return Response(res)
 
     @list_route(methods=['get'])
     def constraint_comparisons(self, request):
-        return Response(dict(GoalMetric.comparisons))
+        res = [
+            {
+                "id": key,
+                "name": value
+            } for key, value in dict(GoalMetric.comparisons).items()
+        ]
+        return Response(res)
