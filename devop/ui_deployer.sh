c6cc79d65f8c167ad953dd214ccbf8b751085eef
Dan <dan.balan0712@gmail.com>
2016-04-29 17:19:33 +0200

    BB-165: Allow different ui versions for dev and v2

--- devop/ui_deployer.sh
+++ devop/ui_deployer.sh
@@ -1,13 +1,22 @@
-pushd repo/betasmartz-ui
-git fetch
-git checkout $1
-docker build -t betasmartz/ui:master_cd .
-docker rm v2_betasmartz_ui_rollback
-docker rename v2_betasmartz_ui v2_betasmartz_ui_rollback
-docker run -d -v /home/bsmartz/ui_dist:/betasmartz-ui/dist --net=betasmartz-local --name=v2_betasmartz_ui betasmartz/ui:master_cd
-#docker exec -it nginx nginx -s reload
-#docker start v2_betasmartz_ui
-docker exec v2_betasmartz_ui bash -c 'export NODE_ENV=production && npm run compile'
-docker stop v2_betasmartz_ui
-popd
+# $1 = commit to checkout
+# $2 = domain to deploy to. Eg. 'v2' or 'demo'
+
+main() {
+    pushd repo/betasmartz-ui
+    git fetch
+    git checkout $1
+    docker build -t betasmartz/ui:${2}_cd .
+    docker rm ${2}_betasmartz_ui_rollback
+    docker rename ${2}_betasmartz_ui ${2}_betasmartz_ui_rollback
+    docker run -d -v /home/bsmartz/ui_dist/${2}:/betasmartz-ui/dist --net=betasmartz-local --name=${2}_betasmartz_ui betasmartz/ui:${2}_cd
+    docker exec ${2}_betasmartz_ui bash -c 'export NODE_ENV=production && npm run compile'
+    docker stop ${2}_betasmartz_ui
+    popd
+}
+
+(
+    flock -w 600 -n 200
+    main $1 $2
+
+) 200>/var/lock/.betasmartz_ui_deployer.exclusivelock
 
