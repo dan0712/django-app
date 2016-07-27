e0f486dd46233771de240b07de13d07add60a8cc
Dan <dan.balan0712@gmail.com>
2016-07-27 16:01:36 -0400

    generate random avatar for user

--- user/connectors.py
+++ user/connectors.py
@@ -1,26 +1,38 @@
-from django.db.models.signals import m2m_changed
-from django.contrib.auth.models import User, Group
+from django.apps import apps  # for D1.8 to get model by name
+from django.contrib.auth.models import User as DjangoUser, Group
+from django.core.files.base import ContentFile
+from django.db.models import signals
 from django.dispatch import receiver
-from django.apps import apps # for D1.8 to get model by name
+
+from main.models import User
+from user.utils import generate_avatar
 
 
-@receiver(m2m_changed)
+@receiver(signals.m2m_changed)
 def create_profiles(sender, instance, model, pk_set, action, **kwargs):
     """
     Create profiles (Advisor, Client, ...)
     for user added to related groups
     """
-    if isinstance(instance, Group) and issubclass(model, User):
-        if action in ['post_add',]:
+    if isinstance(instance, Group) and issubclass(model, DjangoUser):
+        if action in ['post_add', ]:
             group_name = instance.name
-            profile_name = User.PROFILES.get(group_name)
+            profile_name = DjangoUser.PROFILES.get(group_name)
             profile_cls = apps.get_model(**profile_name)
 
             if profile_cls:
                 for pk in pk_set:
-                    user = User.objects.get(pk=pk)
-                    group_profile = profile_cls.objects.get_or_create(user=user)
+                    user = DjangoUser.objects.get(pk=pk)
+                    profile_cls.objects.get_or_create(user=user)
 
     else:
         # it's not a m2m signal we are interested in
         return
+
+
+@receiver(signals.post_save, sender=User)
+def create_avatar(sender: type, instance: User, **kwargs):
+    if not instance.avatar:
+
+        instance.avatar.save('avatar_%d' % instance.id,
+                             ContentFile(generate_avatar(instance.email)))
