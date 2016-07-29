97987018f6513458161608d62f7e68a9613ec9a1
Dan <dan.balan0712@gmail.com>
2016-07-29 11:37:57 -0400

    Added production environment for postgres and nginx

---
+++ devop/nginx/docker_run_production_nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# script to run nginx docker container with mounted volumes on VM server
+# mount deployed environments to the nginx
+docker run --link production_betasmartz_app:production \
+           --link demo_betasmartz_app:demo \
+           -v /home/bsmartz/demo_static:/betasmartz/demo/collected_static \
+           -v /home/bsmartz/demo_media:/betasmartz/demo/media \
+           -v /home/bsmartz/production_media:/betasmartz/production/media \
+           -v /home/bsmartz/production_static:/betasmartz/production/collected_static \
+           -v /home/bsmartz/ui_dist:/betasmartz/ui_dist/ \
+           -v /home/bsmartz/nginx_conf:/etc/nginx/conf.d \
+           -v /home/bsmartz/nginx_ssl:/etc/nginx/ssl \
+           -p 80:80 -p 443:443 --net=betasmartz-local -d --name=nginx nginx:1.9
