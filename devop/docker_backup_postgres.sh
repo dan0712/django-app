3f8c47b6bd045f5c622f29eaa4290a9ae226f8bf
Dan <dan.balan0712@gmail.com>
2016-08-03 20:44:46 -0400

    Cleaned up postgres backup script and made docker compose specifically use 9.5.0 postgres instead of latest to avoid possible conflicts

--- devop/docker_backup_postgres.sh
+++ devop/docker_backup_postgres.sh
@@ -8,45 +8,31 @@
 if env | grep ^PRODUCTION_DBPW= > /dev/null
 then
     echo 'Backing up production database'
-    docker run -it --link postgres:db --net betasmartz-local pg_dump -h db -U betasmartz_production -e PGPASSWORD=${PRODUCTION_DBPW} betasmartz_production > backups/betasmartz_production_latest.dump
-    cd backups
-    gzip betasmartz_production_latest.dump
-    mv betasmartz_production_latest.dump.gz betasmartz_production_$(date +%s).dump.gz
+    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${PRODUCTION_DBPW} pg_dump -Fc -h db -U betasmartz_production betasmartz_production > backups/betasmartz_production_$(date +%s).dump
 else
     # dev machine backups - aus, dev, beta, betastaging, staging
-    cd backups
     # dev
     echo 'Backing up dev database'
-    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${DEV_DB_PASS} pg_dump -h db -U betasmartz_dev betasmartz_dev > betasmartz_dev_latest.dump
-    gzip betasmartz_dev_latest.dump
-    mv betasmartz_dev_latest.dump.gz betasmartz_dev_$(date +%s).dump.gz
+    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${DEV_DB_PASS} pg_dump -Fc -h db -U betasmartz_dev betasmartz_dev > backups/betasmartz_dev_$(date +%s).dump
 
     # beta
     echo 'Backing up beta database'
-    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${BETA_DB_PASS} pg_dump -h db -U betasmartz_beta betasmartz_beta > betasmartz_beta_latest.dump
-    gzip betasmartz_beta_latest.dump
-    mv betasmartz_beta_latest.dump.gz betasmartz_beta_$(date +%s).dump.gz
+    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${BETA_DB_PASS} pg_dump -Fc -h db -U betasmartz_beta betasmartz_beta > backups/betasmartz_beta_$(date +%s).dump
 
     # betastaging
     echo 'Backing up beta staging database'
-    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${BETASTAGING_DB_PASS} pg_dump -h db -U betasmartz_betastaging betasmartz_betastaging > betasmartz_betastaging_latest.dump
-    gzip betasmartz_betastaging_latest.dump
-    mv betasmartz_betastaging_latest.dump.gz betasmartz_betastaging_$(date +%s).dump.gz
+    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${BETASTAGING_DB_PASS} pg_dump -Fc -h db -U betasmartz_betastaging betasmartz_betastaging > backups/betasmartz_betastaging_$(date +%s).dump
 
     # staging
     echo 'Backing up staging database'
-    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${STAGING_DB_PASS} pg_dump -h db -U betasmartz_staging betasmartz_staging > betasmartz_staging_latest.dump
-    gzip betasmartz_staging_latest.dump
-    mv betasmartz_staging_latest.dump.gz betasmartz_staging_$(date +%s).dump.gz
+    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${STAGING_DB_PASS} pg_dump -Fc -h db -U betasmartz_staging betasmartz_staging > backups/betasmartz_staging_$(date +%s).dump
 
     # aus
     echo 'Backing up aus database'
-    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${AUS_DB_PASS} pg_dump -h db -U betasmartz_aus betasmartz_aus > betasmartz_aus_latest.dump
-    gzip betasmartz_aus_latest.dump
-    mv betasmartz_aus_latest.dump.gz betasmartz_aus_$(date +%s).dump.gz
+    docker run -it --link postgres:db --net betasmartz-local -e PGPASSWORD=${AUS_DB_PASS} pg_dump -Fc -h db -U betasmartz_aus betasmartz_aus > backups/betasmartz_aus_$(date +%s).dump
 fi
 
 
 
 # restore betasmartz dump example:
-# pg_restore -U betasmartz_dev -h 127.0.0.1 --verbose --no-owner betasmartz_dev.dump
+# pg_restore -U betasmartz_dev -d betasmartz_dev -h 127.0.0.1 --verbose --no-owner betasmartz_dev.dump
