5a5fb37f9296061060961dc1cb0279f34e36e667
Dan <dan.balan0712@gmail.com>
2016-08-24 08:18:31 +0300

    migrations left behind

---
+++ main/migrations/0046_auto_20160824_1515.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0045_auto_20160821_0150'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='externalassettransfer',
+            name='growth',
+            field=models.FloatField(help_text='Daily rate to increase or decrease the amount by as of the begin_date. 0.0 for no modelled change'),
+        ),
+        migrations.AlterField(
+            model_name='recurringtransaction',
+            name='growth',
+            field=models.FloatField(help_text='Daily rate to increase or decrease the amount by as of the begin_date. 0.0 for no modelled change'),
+        ),
+        migrations.AlterField(
+            model_name='recurringtransaction',
+            name='schedule',
+            field=models.TextField(help_text='RRULE to specify when the transfer happens'),
+        ),
+        migrations.AlterField(
+            model_name='retirementplanatc',
+            name='growth',
+            field=models.FloatField(help_text='Daily rate to increase or decrease the amount by as of the begin_date. 0.0 for no modelled change'),
+        ),
+        migrations.AlterField(
+            model_name='retirementplanbtc',
+            name='growth',
+            field=models.FloatField(help_text='Daily rate to increase or decrease the amount by as of the begin_date. 0.0 for no modelled change'),
+        ),
+        migrations.AlterField(
+            model_name='retirementplaneinc',
+            name='growth',
+            field=models.FloatField(help_text='Daily rate to increase or decrease the amount by as of the begin_date. 0.0 for no modelled change'),
+        ),
+    ]
