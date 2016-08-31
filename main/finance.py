653e6a00ca241f14b57cc321f6c85cac7e3fef9a
Dan <dan.balan0712@gmail.com>
2016-08-31 21:37:54 +0300

    zero balance case

--- main/finance.py
+++ main/finance.py
@@ -20,41 +20,34 @@ def mod_dietz_rate(goals: Iterable) -> float:
             Q(reason=Transaction.REASON_DEPOSIT),
             status=Transaction.STATUS_EXECUTED
         ).order_by('created'))
-    if not transactions:
-        return 0
     for tr in transactions:
         amount = -tr.amount if tr.from_goal is not None else tr.amount
         try:
             cash_flows.append((
                 (tr.created.date() - start_date).days,
-                amount
+                amount,
+                tr.created.date()
             ))
         except TypeError:  # start_date is None
             # use date of the first transaction as opening date
+            # and it's not a part of cashflow
             start_date = tr.created.date()
             begin_value = amount
 
-    cash_flow_balance = sum(i[1] for i in cash_flows)
-    # find all goals with zero balance
-    zero_balanced = [g for g in goals if not g.total_balance]
-    if zero_balanced:
-        # get their last transactions
-        last_transaction_dates = []
-        transaction_list = list(reversed(list(transactions)))
-        for g in zero_balanced:
-            for tr in transaction_list:
-                if tr.from_goal == g or tr.to_goal == g:
-                    last_transaction_dates.append(tr.created.date())
-                    break
-        closing_date = min(last_transaction_dates)
-        # TODO what if some goal got zero balance and some time after
-        # TODO another goal had a transaction
+    if not end_value:  # all goals have zero balance
+        # get closing date and balance from the last transaction
+        _, end_value, closing_date = cash_flows.pop()
+        # last transaction is withdrawal with negative amount,
+        # and balance must be positive
+        end_value = abs(end_value)
     else:
         closing_date = now().date()
+
+    cash_flow_balance = sum(i[1] for i in cash_flows)
     total_days = (closing_date - start_date).days
     prorated_sum = sum(cfi * (total_days - d) / total_days
-                       for d, cfi in cash_flows)
+                       for d, cfi, _ in cash_flows)
     result = (end_value - begin_value -
               cash_flow_balance) / (begin_value + prorated_sum)
-    annualised_return = pow(1 + result, 365.25/total_days) - 1
+    annualised_return = pow(1 + result, 365.25 / total_days) - 1
     return annualised_return
