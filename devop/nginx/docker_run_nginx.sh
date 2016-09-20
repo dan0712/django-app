b36486da73f0f391237bdb3205e2fc0380a73622
Dan <dan.balan0712@gmail.com>
2016-09-19 20:30:11 -0400

    Updated docker run nginx script with demostaging environment

--- devop/nginx/docker_run_nginx.sh
+++ devop/nginx/docker_run_nginx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # script to run nginx docker container with mounted volumes on VM server
 # mount deployed environments to the nginx
-docker run --link prod_betasmartz_app:app \
-           --link demo_betasmartz_app:demo \
+docker run --link demo_betasmartz_app:demo \
            --link dev_betasmartz_app:dev \
            --link v2_betasmartz_app:v2 \
            --link aus_betasmartz_app:aus \
            --link beta_betasmartz_app:beta \
            --link betastaging_betasmartz_app:betastaging \
            --link staging_betasmartz_app:staging \
+           --link demostaging_betasmartz_app:demostaging \
            -v /home/bsmartz/v2_static:/betasmartz/v2/collected_static \
            -v /home/bsmartz/v2_media:/betasmartz/v2/media \
            -v /home/bsmartz/aus_media:/betasmartz/aus/media \
@@ -18,5 +18,3 @@ docker run --link prod_betasmartz_app:app \
            -v /home/bsmartz/nginx_conf:/etc/nginx/conf.d \
            -v /home/bsmartz/nginx_ssl:/etc/nginx/ssl \
            -p 80:80 -p 443:443 --net=betasmartz-local -d --name=nginx nginx:1.9
-
-
