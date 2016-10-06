7715710c6e684022107696c6898700528e103aac
Dan <dan.balan0712@gmail.com>
2016-10-06 10:24:04 +0200

    BU-365: Remove default ETF and AU constraints

--- api/v1/goals/serializers.py
+++ api/v1/goals/serializers.py
@@ -580,20 +580,6 @@ class GoalCreateSerializer(NoUpdateModelSerializer):
                                       rebalance_type=GoalMetric.REBALANCE_TYPE_ABSOLUTE,
                                       rebalance_thr=0.05,
                                       configured_val=recommended_risk)
-            GoalMetric.objects.create(group=metric_group,
-                                      type=GoalMetric.METRIC_TYPE_PORTFOLIO_MIX,
-                                      feature=AssetFeatureValue.Standard.FUND_TYPE_CORE.get_object(),
-                                      comparison=GoalMetric.METRIC_COMPARISON_EXACTLY,
-                                      rebalance_type=GoalMetric.REBALANCE_TYPE_ABSOLUTE,
-                                      rebalance_thr=0.05,
-                                      configured_val=1.0)
-            GoalMetric.objects.create(group=metric_group,
-                                      type=GoalMetric.METRIC_TYPE_PORTFOLIO_MIX,
-                                      feature=AssetFeatureValue.Standard.REGION_AUSTRALIAN.get_object(),
-                                      comparison=GoalMetric.METRIC_COMPARISON_MINIMUM,
-                                      rebalance_type=GoalMetric.REBALANCE_TYPE_ABSOLUTE,
-                                      rebalance_thr=0.05,
-                                      configured_val=0.3)
             if validated_data['ethical']:
                 GoalMetric.objects.create(
                     group=metric_group,
