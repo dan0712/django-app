9e13b6e26de1fecb2416c7f66b680cd0ebbdf665
Dan <dan.balan0712@gmail.com>
2016-10-05 21:39:53 -0400

    Added filter_by_worth and filter_by_risk_level to PositionLot manager

--- main/managers.py
+++ main/managers.py
@@ -199,6 +199,53 @@ class PositionLotQuerySet(QuerySet):
             q |= Q(execution_distribution__transaction__to_goal__account__primary_owner=client)
         return self.filter(q)
 
+    def filter_by_risk_level(self, risk_levels=None):
+        if risk_levels is None:
+            return self
+
+        from .models import GoalMetric
+
+        if isinstance(risk_levels, int):
+            risk_levels = [risk_levels, ]
+        else:
+            risk_levels = [int(r) for r in risk_levels]
+
+        q = Q()
+        for level in risk_levels:
+            risk_min, risk_max = GoalMetric.risk_level_range(level)
+            q |= Q(execution_distribution__transaction__from_goal__selected_settings__metric_group__metrics__configured_val__gte=risk_min,
+                   execution_distribution__transaction__from_goal__selected_settings__metric_group__metrics__configured_val__lt=risk_max)
+            q |= Q(execution_distribution__transaction__to_goal__selected_settings__metric_group__metrics__configured_val__gte=risk_min,
+                   execution_distribution__transaction__to_goal__selected_settings__metric_group__metrics__configured_val__lt=risk_max)
+        qs = self.filter(q, goal__selected_settings__metric_group__metrics__type=GoalMetric.METRIC_TYPE_RISK_SCORE)
+
+        return qs
+
+    def filter_by_worth(self, worth=None):
+        Client = get_model('client', 'Client')
+        qs = self
+        if worth is None:
+            return self
+
+        clients = [p.execution_distribution__transaction__to_goal__account__primary_owner for p in qs]
+        cmap = {}
+        if worth == Client.WORTH_AFFLUENT:
+            cmap['affluent'] = [c.id for c in clients if c.get_worth() == Client.WORTH_AFFLUENT]
+            qs = self.filter(execution_distribution__transaction__to_goal__account__primary_owner__in=cmap['affluent'])
+        elif worth == Client.WORTH_HIGH:
+            cmap['high'] = [c.id for c in clients if c.get_worth() == Client.WORTH_HIGH]
+            qs = self.filter(execution_distribution__transaction__to_goal__account__primary_owner__in=cmap['high'])
+        elif worth == Client.WORTH_VERY_HIGH:
+            cmap['very-high'] = [c.id for c in clients if c.get_worth() == Client.WORTH_VERY_HIGH]
+            qs = self.filter(execution_distribution__transaction__to_goal__account__primary_owner__in=cmap['very-high'])
+        elif worth == Client.WORTH_ULTRA_HIGH:
+            cmap['ultra-high'] = [c.id for c in clients if c.get_worth() == Client.WORTH_ULTRA_HIGH]
+            qs = self.filter(execution_distribution__transaction__to_goal__account__primary_owner__in=cmap['ultra-high'])
+        return qs
+
+
+
+
 
 class ExternalAssetQuerySet(QuerySet):
     def filter_by_user(self, user):
