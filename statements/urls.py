12bd5c1bdda5f14d46f26fc000bc9c3066849032
Dan <dan.balan0712@gmail.com>
2016-11-03 17:16:55 -0400

    Fixed url for retirement soa

--- statements/urls.py
+++ statements/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns(
     '',
     url(r'^records/(?P<pk>\d+)(?P<ext>(\.pdf)?)$', views.RecordView.as_view(), name='record_of_advice'),
     url(r'^(?P<pk>\d+)(?P<ext>(\.\w{3})?)$', views.StatementView.as_view(), name='statement_of_advice'),
-    url(r'^(?P<pk>\d+)(?P<ext>(\.\w{3})?)$', views.RetirementView.as_view(), name='retirement_statement_of_advice'),
+    url(r'^retirement/(?P<pk>\d+)(?P<ext>(\.\w{3})?)$', views.RetirementView.as_view(), name='retirement_statement_of_advice'),
 )
