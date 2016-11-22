9ca08235a9e07cb40de20aed5ab79964f3048371
Dan <dan.balan0712@gmail.com>
2016-11-22 03:15:47 -0500

    MERGE MIGRATIONS

---
+++ client/migrations/0027_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('client', '0024_merge'),
+        ('client', '0026_auto_20161118_0648'),
+    ]
+
+    operations = [
+    ]
