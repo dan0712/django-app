3c1d845003dcf7f94ef869e3cfc045e6bdcaf476
Dan <dan.balan0712@gmail.com>
2016-12-02 15:47:35 -0500

    Code cleanup

--- main/tests/test_legal.py
+++ main/tests/test_legal.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse
-from main import constants
-from api.v1.tests.factories import ClientAccountFactory, \
-    ClientFactory, GoalFactory, \
-    TransactionFactory, AccountTypeRiskProfileGroupFactory, \
-    ExternalAssetFactory, TickerFactory, \
-    SupervisorFactory, AuthorisedRepresentativeFactory, \
-    InvestmentTypeFactory, PositionLotFactory, \
-    ExternalAssetFactory, TickerFactory, \
-    GroupFactory, AdvisorFactory
-from datetime import datetime, date
-from dateutil.relativedelta import relativedelta
-from django.utils import timezone
 from rest_framework import status
-from common.constants import GROUP_SUPPORT_STAFF
 
 
 class PrivacyPolicyTests(TestCase):
